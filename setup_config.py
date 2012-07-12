9dd2020788090bc0b43168c883cdff2ff2783808
alprionPrime <bmosley@alprioncap.com>
2012-07-12 17:09:58 -0400

    setup_win

--- setup_config.py
+++ setup_config.py
@@ -30,3 +30,34 @@ def collect_ext_dirpaths(_dirpath='bg/quantlib'):
                 cython_extension_directories.append(pyxpath)
 
     return cython_extension_directories
+	
+def get_define_macros():
+    defines = [ ('HAVE_CONFIG_H', None)]
+    if sys.platform == 'win32':
+        # based on the SWIG wrappers
+        defines += [
+            (name, None) for name in [
+                '__WIN32__', 'WIN32', 'NDEBUG', '_WINDOWS', 'NOMINMAX', 'WINNT',
+                '_WINDLL', '_SCL_SECURE_NO_DEPRECATE', '_CRT_SECURE_NO_DEPRECATE',
+                '_SCL_SECURE_NO_WARNINGS',
+            ]
+        ]
+    return defines
+
+def get_extra_compile_args():
+    if sys.platform == 'win32':
+        args = ['/GR', '/FD', '/Zm250', '/EHsc' ]
+    else:
+        args = []
+
+    return args
+
+def get_extra_link_args():
+    if sys.platform == 'win32':
+        args = ['/subsystem:windows', '/machine:I386']
+    else:
+        args = []
+
+    return args
+ 
+CYTHON_DIRECTIVES = {"embedsignatur": True}
