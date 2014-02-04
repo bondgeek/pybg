ce9cfc86b511c082c95c71bcbe5c3f8612d2b6b0
Bart Mosley <bartm@bondgeek.com>
2014-02-04 17:47:07 -0500

    working on extension dependencies

--- setup_config.py
+++ setup_config.py
@@ -87,62 +87,14 @@ elif sys.platform == 'win32':
                 extra_link_args = get_extra_link_args(),
                 pyrex_directives= CYTHON_DIRECTIVES
                 )
-extension_paths = [ ('pybg.ql', ['pybg/ql.pyx']),
- ('pybg.settings', ['pybg/settings.pyx']),
- ('pybg.imm', ['pybg/imm.pyx']),
- ('pybg.version', ['pybg/version.pyx']), 
- ('pybg.curves', ['pybg/curves.pyx']),
- ('pybg.curvetypes.usdliborcurve', ['pybg/curvetypes/usdliborcurve.pyx']),
- ('pybg.curvetypes.euriborcurve', ['pybg/curvetypes/euriborcurve.pyx']),
- ('pybg.indexbases', ['pybg/indexbases.pyx']),
- ('pybg.instruments.instrumentbases', ['pybg/instruments/instrumentbases.pyx']),
- ('pybg.instruments.bulletbond', ['pybg/instruments/bulletbond.pyx']),                   
- ('pybg.instruments.callbond', ['pybg/instruments/callbond.pyx']),
- ('pybg.instruments.sinkingfundbond', ['pybg/instruments/sinkingfundbond.pyx']),
- ('pybg.instruments.fixedfloatswap', ['pybg/instruments/fixedfloatswap.pyx']),
- ('pybg.quantlib.currency', ['pybg/quantlib/currency.pyx']), 
- ('pybg.quantlib.cashflow', ['pybg/quantlib/cashflow.pyx']),
- ('pybg.quantlib.quotes', ['pybg/quantlib/quotes.pyx']),
- ('pybg.quantlib.math.optimization', ['pybg/quantlib/math/optimization.pyx']),
- ('pybg.quantlib.time.calendar', ['pybg/quantlib/time/calendar.pyx']),
- ('pybg.quantlib.time.date', ['pybg/quantlib/time/date.pyx']),
- ('pybg.quantlib.time.daycounter', ['pybg/quantlib/time/daycounter.pyx']),
- ('pybg.quantlib.time.schedule', ['pybg/quantlib/time/schedule.pyx']),
- ('pybg.quantlib.time.calendars.germany', ['pybg/quantlib/time/calendars/germany.pyx']),
- ('pybg.quantlib.time.calendars.null_calendar', ['pybg/quantlib/time/calendars/null_calendar.pyx']),
- ('pybg.quantlib.time.calendars.united_kingdom', ['pybg/quantlib/time/calendars/united_kingdom.pyx']),
- ('pybg.quantlib.time.calendars.united_states', ['pybg/quantlib/time/calendars/united_states.pyx']),
- ('pybg.quantlib.time.daycounters.actual_actual', ['pybg/quantlib/time/daycounters/actual_actual.pyx']),
- ('pybg.quantlib.time.daycounters.thirty360', ['pybg/quantlib/time/daycounters/thirty360.pyx']),
- ('pybg.quantlib.index', ['pybg/quantlib/index.pyx']),
- ('pybg.quantlib.indexes.euribor', ['pybg/quantlib/indexes/euribor.pyx']),
- ('pybg.quantlib.indexes.ibor_index', ['pybg/quantlib/indexes/ibor_index.pyx']),
- ('pybg.quantlib.indexes.interest_rate_index', [
-            'pybg/quantlib/indexes/interest_rate_index.pyx']),
- ('pybg.quantlib.indexes.libor', ['pybg/quantlib/indexes/libor.pyx']),
- ('pybg.quantlib.termstructures.volatility.equityfx.black_vol_term_structure', [
-            'pybg/quantlib/termstructures/volatility/equityfx/black_vol_term_structure.pyx'
-            ]),
- ('pybg.quantlib.termstructures.yields.flat_forward', [
-            'pybg/quantlib/termstructures/yields/flat_forward.pyx'
-            ]),
- ('pybg.quantlib.termstructures.yields.piecewise_yield_curve', [
-            'pybg/quantlib/termstructures/yields/piecewise_yield_curve.pyx',
-            'pybg/quantlib/termstructures/yields/_piecewise_support_code.cpp'
-            ]),
- ('pybg.quantlib.termstructures.yields.yield_term_structure', ['pybg/quantlib/termstructures/yields/yield_term_structure.pyx']),
- ('pybg.quantlib.termstructures.yields.zero_curve', ['pybg/quantlib/termstructures/yields/zero_curve.pyx']),
- ('pybg.quantlib.termstructures.yields.rate_helpers', ['pybg/quantlib/termstructures/yields/rate_helpers.pyx'])
 
-]
-
-extension_paths_dbg = [  # use this full set when not debugging
+extension_paths = [  # use this full set when not debugging
  ('pybg.version', ['pybg/version.pyx']),
  ('pybg.ql', ['pybg/ql.pyx']),
  ('pybg.settings', ['pybg/settings.pyx', 'bg/date_utilities.cpp']),
  ('pybg.imm', ['pybg/imm.pyx', 'bg/date_utilities.cpp']),
  ('pybg.curves', ['pybg/curves.pyx', 
-                  'bg/date_utilities.cpp' 
+                  'bg/date_utilities.cpp', 
                   'bg/curvebase.cpp',
                   'bg/curves/ratehelpercurve.cpp',
                   'bg/curves/bondcurve.cpp']),
